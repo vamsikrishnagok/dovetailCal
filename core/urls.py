2ea872b712586719b3c45633ecdb10e4d203b45d
Vamsi <gvamsikrishna.mohan@gmail.com>
2019-08-26 03:56:11 +0530

    initialized core

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,6 @@ from .views import *
 app_name = "core"
 urlpatterns = [
     path('test/', test, name="test"),
-    path('dovetail/', dovetail, name="dovetail")
+    path('', dovetail, name="dovetail")
 
 ]
--- dbbs/urls.py
+++ dbbs/urls.py
@@ -20,5 +20,5 @@ from django.conf import settings
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
-                  path('core/', include('core.urls')),
+                  path('', include('core.urls')),
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
